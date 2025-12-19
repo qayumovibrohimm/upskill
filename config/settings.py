7e41c45e1d27de93d4bf082783bb22551faf3329
Ibrohim <kayumovibrokhim@gmail.com>
2025-12-19 19:27:04 +0500

    env removed

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'kayumovibrokhim@gmail.com'
-EMAIL_HOST_PASSWORD = 'ajji qiph mrde iqvt'
+EMAIL_HOST_USER = 'os.getenv('EMAIL_HOST_USER')'
+EMAIL_HOST_PASSWORD = 'os.getenv('EMAIL_HOST_PASSWORD')'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
